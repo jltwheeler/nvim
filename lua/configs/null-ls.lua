6f61d4726b185e12a08c9e740c7b589b7fc83f35
Josh Wheeler <jltwheeler@gmail.com>
2022-12-19 22:13:07 +0000

    update null ls config

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -12,7 +12,6 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-	null_ls.builtins.formatting.prettier,
 	diagnostics.eslint_d,
 	-- diagnostics.eslint,
 	code_actions.eslint_d,
